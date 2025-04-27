b303e1acd057e7644fd4c797e73a752d32781629
Vladislav Kozlov <littlerock2877@mail.ru>
2025-04-27 22:04:00 +0300

    fix: analyzer

--- telemetry/analyzer/src/main/java/ru/yandex/practicum/analyzer/grpc/HubRouterClient.java
+++ telemetry/analyzer/src/main/java/ru/yandex/practicum/analyzer/grpc/HubRouterClient.java
@@ -1,31 +1,40 @@
-package ru.yandex.practicum.analyzer.controller;
+package ru.yandex.practicum.analyzer.grpc;
 
-import lombok.RequiredArgsConstructor;
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import ru.yandex.practicum.analyzer.model.Action;
-import ru.yandex.practicum.analyzer.model.Scenario;
 import ru.yandex.practicum.grpc.telemetry.event.ActionTypeProto;
 import ru.yandex.practicum.grpc.telemetry.event.DeviceActionProto;
 import ru.yandex.practicum.grpc.telemetry.event.DeviceActionRequest;
 import ru.yandex.practicum.grpc.telemetry.hubrouter.HubRouterControllerGrpc;
 
-@Service
 @Slf4j
-@RequiredArgsConstructor
-public class HubRouterController {
+@Service
+public class HubRouterClient {
+
     private final HubRouterControllerGrpc.HubRouterControllerBlockingStub hubRouterClient;
 
-    public void doAction(Scenario scenario, Action action, String hubId) {
+    public HubRouterClient(@Value("${grpc.client.hub-router.address}") String address) {
+        ManagedChannel channel = ManagedChannelBuilder.forTarget(address)
+                .usePlaintext()
+                .build();
+        this.hubRouterClient = HubRouterControllerGrpc.newBlockingStub(channel);
+    }
+
+    public void executeAction(Action action, String hubId) {
         DeviceActionRequest request = DeviceActionRequest.newBuilder()
                 .setHubId(hubId)
-                .setScenarioName(scenario.getName())
+                .setScenarioName(action.getScenario().getName())
                 .setAction(DeviceActionProto.newBuilder()
                         .setSensorId(action.getSensorId())
                         .setType(ActionTypeProto.valueOf(action.getType().name()))
                         .setValue(action.getValue())
                         .build())
                 .build();
+
         try {
             hubRouterClient.handleDeviceAction(request);
             log.info("Sent action to hub: {}", action);
