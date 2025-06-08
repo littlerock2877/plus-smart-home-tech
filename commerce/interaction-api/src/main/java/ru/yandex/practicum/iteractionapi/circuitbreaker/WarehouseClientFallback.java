79acedf74e02405dfb8255960937fd7a55b6240a
Vladislav Kozlov <littlerock2877@mail.ru>
2025-06-08 13:53:35 +0300

    fix: compillation

--- commerce/interaction-api/src/main/java/ru/yandex/practicum/iteractionapi/circuitbreaker/WarehouseClientFallback.java
+++ commerce/interaction-api/src/main/java/ru/yandex/practicum/iteractionapi/circuitbreaker/WarehouseClientFallback.java
@@ -2,12 +2,17 @@ package ru.yandex.practicum.iteractionapi.circuitbreaker;
 
 import org.springframework.stereotype.Component;
 import ru.yandex.practicum.iteractionapi.dto.AddressDto;
+import ru.yandex.practicum.iteractionapi.dto.AssemblyProductsForOrderRequest;
 import ru.yandex.practicum.iteractionapi.dto.BookedProductsDto;
+import ru.yandex.practicum.iteractionapi.dto.ShippedToDeliveryRequest;
 import ru.yandex.practicum.iteractionapi.dto.ShoppingCartDto;
 import ru.yandex.practicum.iteractionapi.client.WarehouseClient;
 import ru.yandex.practicum.iteractionapi.exception.WarehouseServerUnavailableException;
 import ru.yandex.practicum.iteractionapi.request.AddProductToWarehouseRequest;
 import ru.yandex.practicum.iteractionapi.request.NewProductInWarehouseRequest;
+
+import java.util.Map;
+import java.util.UUID;
 
 @Component
 public class WarehouseClientFallback implements WarehouseClient {
@@ -34,6 +39,26 @@ public class WarehouseClientFallback implements WarehouseClient {
 
     @Override
     public BookedProductsDto bookingCartProducts(ShoppingCartDto shoppingCartDto) {
+        throw new WarehouseServerUnavailableException("Fallback response: сервис временно недоступен.");
+    }
+
+    @Override
+    public AddressDto getWarehouseAddress() {
+        throw new WarehouseServerUnavailableException("Fallback response: сервис временно недоступен.");
+    }
+
+    @Override
+    public void addReturnProductInWarehouse(Map<UUID, Long> products) {
+        throw new WarehouseServerUnavailableException("Fallback response: сервис временно недоступен.");
+    }
+
+    @Override
+    public BookedProductsDto createAssembly(AssemblyProductsForOrderRequest assembly) {
+        throw new WarehouseServerUnavailableException("Fallback response: сервис временно недоступен.");
+    }
+
+    @Override
+    public void shippedToDelivery(ShippedToDeliveryRequest deliveryRequest) {
         throw new WarehouseServerUnavailableException("Fallback response: сервис временно недоступен.");
     }
 }
